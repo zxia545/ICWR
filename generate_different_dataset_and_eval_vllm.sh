e574ba9f25b3e7e146071e0808f9e8656eb597f9
zxia545 <xiaozheyuan72@gmail.com>
2024-06-18 00:10:59 +1200

    Fixed the naming of sh script

--- generate_different_dataset_and_eval_vllm.sh
+++ generate_different_dataset_and_eval_vllm.sh
@@ -7,8 +7,10 @@ SCRIPT_DIR=$(pwd)
 # Update these API keys with your actual keys
 API_KEYS="sk-yourfirstkey,sk-yoursecondkey,sk-yourthirdkey,..."
 
-# Enter the openai model name
-GEN_MODEL="gpt-3.5-turbo-0125"
+# Enter the vllm hosted model name
+VLLM_HOST_MODEL_NAME="llama3"
+VLLM_PORT_NUMBER=8010
+
 # Generation MODE
 # NOTE available modes: "default", "detailed", "concise", "quality_enhancement", "copy_paste"
 MODE="copy_paste"
@@ -31,15 +33,15 @@ mkdir -p $OUTPUT_FOLDER
 for dataset_name in 'koala' 'vicuna' 'sinstruct' 'wizardlm' 'lima' 'alpaca'
 do
 INPUT_JSON=./reference/generation_template/${dataset_name}_template.json
-python ./generation/generate_different_dataset_gpt.py \
+python ./generation/generate_different_dataset_vllm.py \
     --input_json_path $INPUT_JSON \
-    --output_json_path ${OUTPUT_FOLDER}/${dataset_name}_${GEN_MODEL}_${MODE}.json \
+    --output_json_path ${OUTPUT_FOLDER}/${dataset_name}_${VLLM_HOST_MODEL_NAME}_${MODE}.json \
     --input_dataset ${dataset_name} \
-    --api_keys $API_KEYS \
     --max_workers $MAX_WORKERS \
-    --model_name ${GEN_MODEL} \
-    --model ${GEN_MODEL} \
-    --mode ${MODE}
+    --model_name ${VLLM_HOST_MODEL_NAME} \
+    --model ${VLLM_HOST_MODEL_NAME} \
+    --mode ${MODE} \
+    --port ${VLLM_PORT_NUMBER}
 done
 
 
